a49686bb5b79bbf95220edf220ec6669920163aa
SunsetFrost <1262457596@qq.com>
2022-01-09 09:37:28 +0800

    Add SEO info

--- pages/essay/[slug].tsx
+++ pages/essay/[slug].tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import Image from "next/image";
-import fs from "fs";
+import Head from "next/head";
 import matter from "gray-matter";
 import { getPostSlugs, getPostString } from "../../lib/api";
 import { EssayInfo } from "../../types/essay";
@@ -12,28 +12,42 @@ interface IProps {
 
 const Essay: FunctionComponent<IProps> = ({ essay }) => {
   return (
-    <div className="flex flex-col align-middle w-screen h-screen">
-      <div className="relative w-screen h-44 lg:h-80">
-        <div className="w-full h-44 lg:h-80 bg-gradient-to-br from-cyan-200 to-blue-500">
-          <Image
-            className="object-cover object-center opacity-30"
-            src={essay.meta.thumbnail}
-            alt="blog tumbnail"
-            layout="fill"
-          />
+    <>
+      <Head>
+        <title>{essay.meta.title}</title>
+        <meta name="description" content={essay.meta.description} />
+        <meta property="og:title" content={essay.meta.title} />
+        <meta property="og:description" content={essay.meta.description} />
+        <meta
+          property="og:url"
+          content={`https://sunsetfrost.art/essay/${essay.meta.slug}`}
+        />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col align-middle w-screen h-screen">
+        <div className="relative w-screen h-44 lg:h-80">
+          <div className="w-full h-44 lg:h-80 bg-gradient-to-br from-cyan-200 to-blue-500">
+            <Image
+              className="object-cover object-center opacity-30"
+              src={essay.meta.thumbnail}
+              alt="blog tumbnail"
+              layout="fill"
+            />
+          </div>
+
+          <div className="absolute top-0 left-0 w-full">
+            <h1 className="text-slate-50 w-4/5 text-center font-bold  text-lg lg:text-3xl mx-auto mt-10 lg:mt-20">
+              {essay.meta.title}
+            </h1>
+          </div>
         </div>
 
-        <div className="absolute top-0 left-0 w-full">
-          <h1 className="text-slate-50 w-4/5 text-center font-bold  text-lg lg:text-3xl mx-auto mt-10 lg:mt-20">
-            {essay.meta.title}
-          </h1>
+        <div className="w-11/12 lg:max-w-6xl -mt-10 z-10 py-8 px-4 lg:px-12 mx-auto bg-white rounded-xl shadow-xl">
+          <Markdown content={essay.content} />
         </div>
       </div>
-
-      <div className="w-11/12 lg:max-w-6xl -mt-10 z-10 py-8 px-4 lg:px-12 mx-auto bg-white rounded-xl shadow-xl">
-        <Markdown content={essay.content} />
-      </div>
-    </div>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { FunctionComponent } from "react";
 import type { GetStaticProps } from "next";
+import Head from "next/head";
 import matter from "gray-matter";
+import { Pagination, Navigation, Scrollbar, A11y, Mousewheel } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import { getPostSlugs, getPostString } from "../lib/api";
 import { EssayMeta } from "../types/essay";
 import EssayCard from "./components/card";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Scrollbar, A11y, Mousewheel } from "swiper";
+import Meta from "./components/meta";
 
 export interface IProps {
   essays: EssayMeta[];
@@ -14,6 +17,17 @@ export interface IProps {
 const Home: FunctionComponent<IProps> = ({ essays }) => {
   return (
     <div className="w-screen h-screen bg-gradient-to-br from-cyan-400 to-blue-500 flex shadow-xl">
+      <Meta />
+      <Head>
+        <title>Sunset Frost Blog</title>
+        <meta property="og:title" content="Sunset Frost Blog" />
+        <meta
+          property="og:description"
+          content="Sunset Frost's Blog. Life needs to have some meaning."
+        />
+        <meta property="og:url" content="https://sunsetfrost.art/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Swiper
         className="p-6 w-10/12 lg:w-3/5 h-4/5  lg:h-2/4 m-auto bg-white rounded-xl shadow-lg"
         spaceBetween={30}
@@ -28,7 +42,13 @@ const Home: FunctionComponent<IProps> = ({ essays }) => {
       >
         {essays.map((essay, index) => (
           <SwiperSlide key={index} className="">
-            <EssayCard date={essay.date} title={essay.title} description={essay.description} thumbnail={essay.thumbnail} slug={essay.slug} />
+            <EssayCard
+              date={essay.date}
+              title={essay.title}
+              description={essay.description}
+              thumbnail={essay.thumbnail}
+              slug={essay.slug}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
